2f1ff3d8302321b0026960e1d7f035715da292e1
eugen-polanski <awdejtschyk@gmail.com>
2021-09-30 21:51:14 +0300

    home_work_1

---
+++ task_1.py
@@ -0,0 +1,5 @@
+dog = "Шарик"
+action = "Играет"
+toy = "Мячик"
+
+print(dog + ' ' + action + ' в ' + toy)
---
+++ task_2.py
@@ -0,0 +1,9 @@
+name = "Василий"
+surname = "Иванов"
+age = 33
+height = 182
+
+birth_year = 2021 - age
+
+bio = name + ' ' + surname + ' ' + str(birth_year) + ' ' + ("ростом ") + str(height) + (" см")
+print(bio)
